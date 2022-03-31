3b452fccd913a3fb62ab91a9811fc11f4cb32787
Tloluvin <tloluvin.cbc@gmail.com>
2022-03-31 09:26:54 +0000

    if upgrade

--- ExclamationMarks.py
+++ ExclamationMarks.py
@@ -1,9 +1,10 @@
 # Remove an exclamation marks from the end of string
 def remove(s):
-    if len(s) > 0 and (s[-1] == '!'):
+    if s[-1:] == '!':
         s = s[:-1]
         return remove(s)
     return s
+
 
 print(remove('Hi'))
 print(remove('Hi!!!'))
