5f27e94fe4c822134280cfb9b953fdc887d4f169
Tloluvin <tloluvin.cbc@gmail.com>
2022-04-09 19:36:26 +0000

    Given two words and a letter, return a single word

--- StringMerge.py
+++ StringMerge.py
@@ -5,3 +5,13 @@
 # ("jason", "samson", "s")      ==>  "jasamson"
 # ("wonderful", "people", "e")  ==>  "wondeople"
 
+def string_merge(string1, string2, letter):
+    index1 = string1.index(letter)
+    index2 = string2.index(letter)
+    return string1[0:index1] + string2[index2:]
+
+
+print(string_merge("hello", "world", "l"))      # held
+print(string_merge("coding", "anywhere", "n"))  # codinywhere"
+print(string_merge("jason", "samson", "s"))     # jasamson
+print(string_merge("wonderful", "people", "e"))  # wondeople
