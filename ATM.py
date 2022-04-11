d895493b27eb9810b583fb47cb1ba9ebc8af0884
Tloluvin <tloluvin.cbc@gmail.com>
2022-04-11 10:17:33 +0000

    Task

---
+++ ATM.py
@@ -0,0 +1,16 @@
+# An ATM has banknotes of nominal values 10, 20, 50, 100, 200 and 500 dollars. You can consider that there is a large enough supply of each of these banknotes.
+# You have to write the ATM's function that determines the minimal number of banknotes needed to honor a withdrawal of n dollars, with 1 <= n <= 1500.
+# Return that number, or -1 if it is impossible.
+
+def solve(n):
+    pass
+
+
+print(solve(770))     # 4
+print(solve(550))     # 2
+print(solve(10))      # 1
+print(solve(1250))    # 4
+print(solve(1500))    # 3
+print(solve(125))     # -1
+print(solve(666))     # -1
+print(solve(42))      # -1
