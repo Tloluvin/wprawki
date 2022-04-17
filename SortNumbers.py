26bc038a7379669b4913994ec58b642b49042236
Tloluvin <tloluvin.cbc@gmail.com>
2022-04-17 06:51:42 +0000

    Refactor

--- SortNumbers.py
+++ SortNumbers.py
@@ -1,10 +1,7 @@
 # Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
 
 def solution(nums):
-    result = []
-    if nums == None:
-        return []
-    return sorted(nums)
+    return sorted(nums) if nums != None else []
 
 
 print(solution([1,2,3,10,5]))   # should return [1,2,3,5,10]
