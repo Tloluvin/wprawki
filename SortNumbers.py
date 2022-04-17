825db46242c9a9f66163d9f8529b745d339ad342
Tloluvin <tloluvin.cbc@gmail.com>
2022-04-17 06:38:48 +0000

    Task

---
+++ SortNumbers.py
@@ -0,0 +1,12 @@
+# Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+def solution(nums):
+    pass
+
+
+
+print(solution([1,2,3,10,5]))   # should return [1,2,3,5,10]
+print(solution(None))           # should return []
+print(solution([]))             # should return []
+print(solution([20,2,10]))      # should return [2,10,20]
+print(solution([2,20,10]))      # should return [2,10,20]
