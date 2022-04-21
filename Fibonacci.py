df1e9fe6be885bb6e74e8bc2234fa5730bfa4638
Tloluvin <tloluvin.cbc@gmail.com>
2022-04-21 15:07:00 +0000

    solution

--- Fibonacci.py
+++ Fibonacci.py
@@ -3,7 +3,13 @@
 def fibonacci(n: int) -> int:
     """Given a positive argument n, returns the nth term of the Fibonacci Sequence.
     """
-    return 0
+    result = 0
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
 print(fibonacci(0))     # 0
@@ -13,6 +19,6 @@ print(fibonacci(3))     # 2
 print(fibonacci(4))     # 3
 print(fibonacci(5))     # 5
 print(fibonacci(6))     # 8
-print(fibonacci(34))    # 3
+print(fibonacci(34))    # 5702887
 print(fibonacci(299))   # 137347080577163115432025771710279131845700275212767467264610201
 
